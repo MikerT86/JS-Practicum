39efd7f4b0b84866ae6204bcff08906761de669e
Mike <mternyuk@gmail.com>
2019-04-28 21:19:50 +0300

    Refactoring

--- JS_course/CurrencyConvertor/JS/script.js
+++ JS_course/CurrencyConvertor/JS/script.js
@@ -1,21 +1,37 @@
+'use strict';
+
 let inputRub    = document.getElementById('rub'),
     inputValue  = document.getElementById('currency-input'),
     selectorCur = document.querySelector('.currency');
 
-function evaluateCurrency() {
-    let request = new XMLHttpRequest();
+let promiseHandler = function() {
 
-    // request.open(method, url, async, login, pass); 
-
-    request.open('GET', 'JS/currency.json');
-    request.setRequestHeader('Content-type', 'application/json; charset: utf-8');
-    request.send();
-
-    request.addEventListener('readystatechange', function() {
+    let evaluateCurrency = function() {
     
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+        let promise = new Promise((resolve, reject) => {
+        
+            let request = new XMLHttpRequest();
+        
+            request.open('GET', 'JS/currency.json');
+            request.setRequestHeader('Content-type', 'application/json; charset: utf-8');
+        
+            request.onload = () => {
+                resolve(JSON.parse(request.response));
+            };
+            request.onerror = () => {
+                reject('error');
+            };
+    
+            request.send();
+    
+        });
+    
+        return promise;
+        
+    };     
 
+    evaluateCurrency()
+        .then(data => {
             switch (selectorCur.value) {
                 case 'USD':
                     inputValue.value = (inputRub.value / data.usd).toFixed(2);
@@ -30,12 +46,12 @@ function evaluateCurrency() {
                     inputValue.value = 'currency pick';
                     break;
             }
+        })
+        .then(() => {console.log(`За ${inputRub.value} RUB, вы получите ${inputValue.value} ${selectorCur.value}`)})
+        .catch(error => {
+            inputValue.value = error;   
+        });
+};
 
-        } else {
-            inputValue.value = "ERROR";
-        }
-    })   
-}
-
-inputRub.addEventListener('input', evaluateCurrency);
-selectorCur.addEventListener('change', evaluateCurrency);
+inputRub.addEventListener('change', promiseHandler);
+selectorCur.addEventListener('change', promiseHandler);
